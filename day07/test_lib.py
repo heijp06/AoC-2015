c331690d219a383163554b131788011809ab7c12
Peter van der Heijden <peter@vanderheijden.name>
2021-10-16 16:21:04 +0200

    Add additional test for corner cases.

--- day07/test_lib.py
+++ day07/test_lib.py
@@ -6,33 +6,51 @@ def test_d():
     circuit = Circuit(gates)
     assert circuit.value("d") == 72
 
+
 def test_e():
     circuit = Circuit(gates)
     assert circuit.value("e") == 507
+
 
 def test_f():
     circuit = Circuit(gates)
     assert circuit.value("f") == 492
 
+
 def test_g():
     circuit = Circuit(gates)
     assert circuit.value("g") == 114
+
 
 def test_h():
     circuit = Circuit(gates)
     assert circuit.value("h") == 65412
 
+
 def test_i():
     circuit = Circuit(gates)
     assert circuit.value("i") == 65079
+
 
 def test_x():
     circuit = Circuit(gates)
     assert circuit.value("x") == 123
 
+
 def test_y():
     circuit = Circuit(gates)
     assert circuit.value("y") == 456
+
+
+def test_odd_gate():
+    circuit = Circuit(["3 -> b", "1 AND b -> a"])
+    assert circuit.value("a") == 1
+
+
+def test_copy_gate():
+    circuit = Circuit(["3 -> b", "b -> a"])
+    assert circuit.value("a") == 3
+
 
 gates = [
     "123 -> x",
