1d508bc1455e4c95dbbc81a9a034b335be112beb
Peter van der Heijden <peter@vanderheijden.name>
2021-10-17 21:03:10 +0200

    Solve day 13 part 2.

--- day13/lib.py
+++ day13/lib.py
@@ -1,20 +1,22 @@
 import re
 from itertools import permutations
 
+
 def part1(rows):
     seating = Seating(rows)
     return seating.optimal()
 
 
 def part2(rows):
-    pass
+    seating = Seating(rows, "me")
+    return seating.optimal()
 
 
 class Seating:
-    def __init__(self, guest_list):
+    def __init__(self, guest_list, me=None):
         self.guests = set()
         self.happiness = {}
-        self.first_guest = None
+        self.first_guest = me
         for row in guest_list:
             match = re.match(
                 r"(.*) would (.*) (.*) happiness units by sitting next to (.*)\.",
@@ -27,6 +29,14 @@ class Seating:
                     self.guests.add(guest1)
             self.happiness[guest1, guest2] = int(
                 units) if sign == "gain" else -int(units)
+        self.add_me(me)
+
+    def add_me(self, me):
+        if not me:
+            return
+        for guest in self.guests:
+            self.happiness[me, guest] = 0
+            self.happiness[guest, me] = 0
 
     def optimal(self):
         return max(
@@ -37,7 +47,7 @@ class Seating:
             )
             for arrangement in self.arrangements()
         )
-    
+
     def arrangements(self):
         for permutation in permutations(self.guests):
             yield [self.first_guest, *permutation, self.first_guest]
