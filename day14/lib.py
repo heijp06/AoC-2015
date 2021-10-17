e0cd4e87b0f2461a45e118f8e6ae62e87a5df886
Peter van der Heijden <peter@vanderheijden.name>
2021-10-17 22:39:09 +0200

    Make a Herd class.

--- day14/lib.py
+++ day14/lib.py
@@ -2,23 +2,15 @@ import re
 
 
 def part1(rows, seconds=2503):
-    herd = [Reindeer(row) for row in rows]
-    for _ in range(seconds):
-        for reindeer in herd:
-            reindeer.move()
-    return max(reindeer.position for reindeer in herd)
+    herd = Herd(rows)
+    herd.race(seconds)
+    return herd.max_position()
 
 
 def part2(rows, seconds=2503):
-    herd = [Reindeer(row) for row in rows]
-    for _ in range(seconds):
-        for reindeer in herd:
-            reindeer.move()
-        lead = max(reindeer.position for reindeer in herd)
-        for reindeer in herd:
-            if reindeer.position == lead:
-                reindeer.points += 1
-    return max(reindeer.points for reindeer in herd)
+    herd = Herd(rows)
+    herd.race(seconds)
+    return herd.max_points()
 
 
 class Reindeer:
@@ -50,3 +42,25 @@ class Reindeer:
         elif self.timer == 0:
             self.state = Reindeer.FLYING
             self.timer = self.fly_time
+
+
+class Herd:
+    def __init__(self, rows):
+        self.reindeer = [Reindeer(row) for row in rows]
+
+    def move(self):
+        for reindeer in self.reindeer:
+            reindeer.move()
+        for reindeer in self.reindeer:
+            if reindeer.position == self.max_position():
+                reindeer.points += 1
+
+    def max_position(self):
+        return max(reindeer.position for reindeer in self.reindeer)
+
+    def max_points(self):
+        return max(reindeer.points for reindeer in self.reindeer)
+
+    def race(self, seconds):
+        for _ in range(seconds):
+            self.move()
