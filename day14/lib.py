01624d26ff3168b35a34f39fb51a66352324a595
Peter van der Heijden <peter@vanderheijden.name>
2021-10-17 22:16:22 +0200

    Solve day 14 part 2.

--- day14/lib.py
+++ day14/lib.py
@@ -2,19 +2,30 @@ import re
 
 
 def part1(rows, seconds=2503):
-    return max(Reindeer(row).position(seconds) for row in rows)
+    herd = [Reindeer(row) for row in rows]
+    for _ in range(seconds):
+        for reindeer in herd:
+            reindeer.move()
+    return max(reindeer.position for reindeer in herd)
 
 
-
-def part2(rows):
-    pass
+def part2(rows, seconds=2503):
+    herd = [Reindeer(row) for row in rows]
+    for _ in range(seconds):
+        for reindeer in herd:
+            reindeer.move()
+        lead = max(reindeer.position for reindeer in herd)
+        for reindeer in herd:
+            if reindeer.position == lead:
+                reindeer.points += 1
+    return max(reindeer.points for reindeer in herd)
 
 
 class Reindeer:
-    def __init__(self, row):
-        self.parse(row)
+    FLYING = 0
+    RESTING = 1
 
-    def parse(self, row):
+    def __init__(self, row):
         match = re.match(
             r"(.*) can fly (.*) km/s for (.*) seconds, but then must rest for (.*) seconds.",
             row
@@ -24,7 +35,18 @@ class Reindeer:
         self.speed = int(speed)
         self.fly_time = int(fly_time)
         self.rest_time = int(rest_time)
-    
-    def position(self, seconds):
-        quotient, remainder = divmod(seconds, self.fly_time + self.rest_time)
-        return (quotient * self.fly_time + min(self.fly_time, remainder)) * self.speed
+        self.state = Reindeer.FLYING
+        self.timer = self.fly_time
+        self.position = 0
+        self.points = 0
+
+    def move(self):
+        self.timer -= 1
+        if self.state == Reindeer.FLYING:
+            self.position += self.speed
+            if self.timer == 0:
+                self.state = Reindeer.RESTING
+                self.timer = self.rest_time
+        elif self.timer == 0:
+            self.state = Reindeer.FLYING
+            self.timer = self.fly_time
--- day14/test_lib.py
+++ day14/test_lib.py
@@ -8,17 +8,31 @@ def test_parse():
     assert reindeer.speed == 14
     assert reindeer.fly_time == 10
     assert reindeer.rest_time == 127
+    assert reindeer.state == Reindeer.FLYING
+    assert reindeer.timer == 10
+    assert reindeer.position == 0
+    assert reindeer.points == 0
+
 
 def test_position_1_second():
     comet = Reindeer(example[0])
-    assert comet.position(1) == 14
+    comet.move()
+    assert comet.position == 14
+
 
 def test_position_1000_second():
     comet = Reindeer(example[0])
-    assert comet.position(1000) == 1120
+    for _ in range(1000):
+        comet.move()
+    assert comet.position == 1120
+
 
 def test_part1():
     assert part1(example, 1000) == 1120
+
+
+def test_part2():
+    assert part2(example, 1000) == 689
 
 
 example = [
