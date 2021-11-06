8c237541139b5e41aec8348442c7a50d7dacc59f
Peter van der Heijden <peter@vanderheijden.name>
2021-11-06 18:05:31 +0100

    Solve day 17 part 2.

--- day16/lib.py
+++ day16/lib.py
@@ -1,25 +1,36 @@
 import re
+import operator
 
 
 def part1(rows):
     for i in range(len(rows)):
-        if all(ticker_tape[m.group(1)] == m.group(2) for m in re.finditer("([a-z]+): (\d+)", rows[i])):
+        if all(
+                int(m.group(2)) == ticker_tape[m.group(1)][0]
+                for m
+                in re.finditer("([a-z]+): (\d+)", rows[i])
+            ):
             return i+1
 
 
 def part2(rows):
-    pass
+    for i in range(len(rows)):
+        if all(
+                ticker_tape[m.group(1)][1](int(m.group(2)), ticker_tape[m.group(1)][0])
+                for m
+                in re.finditer("([a-z]+): (\d+)", rows[i])
+            ):
+            return i+1
 
 
 ticker_tape = {
-    "children": "3",
-    "cats": "7",
-    "samoyeds": "2",
-    "pomeranians": "3",
-    "akitas": "0",
-    "vizslas": "0",
-    "goldfish": "5",
-    "trees": "3",
-    "cars": "2",
-    "perfumes": "1"
+    "children": (3, operator.eq),
+    "cats": (7, operator.gt),
+    "samoyeds": (2, operator.eq),
+    "pomeranians": (3, operator.lt),
+    "akitas": (0, operator.eq),
+    "vizslas": (0, operator.eq),
+    "goldfish": (5, operator.lt),
+    "trees": (3, operator.gt),
+    "cars": (2, operator.eq),
+    "perfumes": (1, operator.eq)
 }
