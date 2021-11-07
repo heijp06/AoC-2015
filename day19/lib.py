fd5be849be2ce3e0719a3a518f2439095da952b4
Peter van der Heijden <peter@vanderheijden.name>
2021-11-07 21:06:39 +0100

    Simplify code for part 1.

--- day19/lib.py
+++ day19/lib.py
@@ -1,24 +1,37 @@
 import re
 from collections import defaultdict
 
+
 def part1(rows):
-    molecule = rows[-1]
-    molecules = set()
-    for row in rows:
-        match = re.match("(\w+) => (\w+)", row)
-        if match:
-            pattern = match.group(1)
-            replacement = match.group(2)
-            start = 0
-            pos = molecule.find(pattern, start)
-            while pos >= 0:
-                new_molecule = molecule[:pos] + replacement + molecule[pos + len(pattern):]
-                molecules.add(new_molecule)
-                start = pos + 1
-                pos = molecule.find(pattern, start)
+    medicine = rows[-1]
+    replacements = get_replacements(rows)
+    molecules = replace_all(medicine, replacements)
     return len(molecules)
 
 
-
+# Once created, Ar, C, Rn, Th and Y can never be destroyed.
 def part2(rows):
     pass
+
+
+def get_replacements(rows):
+    replacements = []
+    for row in rows:
+        match = re.match("(\w+) => (\w+)", row)
+        if match:
+            replacements.append((match.group(1), match.group(2)))
+    return replacements
+
+
+def replace_all(molecule, replacements):
+    new_molecules = set()
+    for pattern, replacement in replacements:
+        start = 0
+        pos = molecule.find(pattern, start)
+        while pos >= 0:
+            new_molecule = molecule[:pos] + \
+                replacement + molecule[pos + len(pattern):]
+            new_molecules.add(new_molecule)
+            start = pos + len(pattern)
+            pos = molecule.find(pattern, start)
+    return new_molecules
