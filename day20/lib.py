eef2a55679f38a524087d43ff8d82bff74d7769b
Peter van der Heijden <peter@vanderheijden.name>
2021-11-27 20:48:55 +0100

    Solve day 20 part 2.

--- day20/lib.py
+++ day20/lib.py
@@ -1,15 +1,25 @@
 SIZE = 1000000
 
-def part1(presents, size = SIZE):
-    target = presents // 10
+
+def part1(presents, size=SIZE):
     sieve = [0] * size
     for elf in range(1, size):
+        present = 10 * elf
         for house in range(elf, size, elf):
-            sieve[house] += elf
+            sieve[house] += present
     for elf in range(1, size):
-        if sieve[elf] >= target:
+        if sieve[elf] >= presents:
             return elf
     return -1
 
-def part2(rows):
-    pass
+
+def part2(presents, size=SIZE):
+    sieve = [0] * size
+    for elf in range(1, size):
+        present = 11 * elf
+        for house in range(elf, min(size, 50 * elf), elf):
+            sieve[house] += present
+    for elf in range(1, size):
+        if sieve[elf] >= presents:
+            return elf
+    return -1
