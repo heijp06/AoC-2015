eeb02486ce4ee797473c31d17dc2982c9a9e4fd2
Peter van der Heijden <peter@vanderheijden.name>
2021-11-27 14:27:05 +0100

    Solve day 19 part 2

--- day19/test_lib.py
+++ day19/test_lib.py
@@ -1,90 +1,7 @@
-import pytest
-from lib import part1, part2, reduce
-from group import Group
+from lib import reduce
 
 
-def test_molecule_h_f():
-    assert Group.parse_molecule("HF") == Group(["HF", ])
-
-
-def test_molecule_c_rn_al_ar_f():
-    assert Group.parse_molecule("CRnAlArF") == Group(["C", Group(["Al"]), "F"])
-
-
-def test_molecule_c_rn_al_ar():
-    assert Group.parse_molecule("CRnAlAr") == Group(["C", Group(["Al"])])
-
-
-def test_molecule_rn_al_ar_f():
-    assert Group.parse_molecule("RnAlArF") == Group([Group(["Al"]), "F"])
-
-
-def test_molecule_rn_rn_ar_ar():
-    assert Group.parse_molecule("RnRnArAr") == Group([Group([Group([])])])
-
-
-def test_reduce_rn_b_p_ti_mg_ar():
-    group = Group.parse_molecule("BPTiMg")
-
-    assert len(group.groups) == 1
-    assert group.groups[0] == "BPTiMg"
-
-    group.reduce(replacements)
-
-    assert group.reductions == 3
-    assert len(group.groups) == 1
-
-
-def test_reduce_h_si_rn_mg_ar():
-    group = Group.parse_molecule("HSiRnMgAr")
-
-    assert len(group.groups) == 2
-    assert group.groups[0] == "HSi"
-    assert group.groups[1] == Group(["Mg"])
-
-    group.reduce(replacements, {"H"})
-
-    assert group.reductions == 2
-    assert len(group.groups) == 1
-    assert group.groups[0] == "H"
-
-
-def test_reduce_c_rn_f_y_f_ar():
-    group = Group.parse_molecule("CRnFYFAr")
-
-    assert len(group.groups) == 2
-    assert group.groups[0] == "C"
-    assert group.groups[1] == Group(["FYF"])
-
-    group.reduce(replacements, {"O"})
-
-    assert group.reductions == 1
-    assert len(group.groups) == 1
-    assert group.groups[0] == "O"
-
-
-def test_reduce_c_rn_f_ar_th_rn_f_ar():
-    group = Group.parse_molecule("CRnFArThRnFAr")
-
-    assert len(group.groups) == 4
-    assert group.groups[0] == "C"
-    assert group.groups[1] == Group(["F"])
-    assert group.groups[2] == "Th"
-    assert group.groups[3] == Group(["F"])
-
-    group.reduce(replacements, {"e"})
-
-    assert group.reductions == 3
-    assert len(group.groups) == 1
-    assert group.groups[0] == "e"
-
-
-def test_group_replace_all():
-    assert Group._replace_all("BPTiMg", replacements) == {
-        "TiTiMg", "BPMg"}
-
-
-def test_new_reduce_c_rn_f_ar_th_rn_f_ar():
+def test_reduce():
     assert reduce("CRnFArThRnFAr", replacements) == 3
 
 
@@ -133,19 +50,3 @@ replacements = [
     ("NAl", "e"),
     ("OMg", "e")
 ]
-
-terminals = {
-    "Al",
-    "B",
-    "Ca",
-    "F",
-    "H",
-    "Mg",
-    "N",
-    "O",
-    "P",
-    "Si",
-    "Th",
-    "Ti",
-    "e"
-}
